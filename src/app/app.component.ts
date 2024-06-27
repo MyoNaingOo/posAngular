2cf4951fed2cb338360d75bb166e57ac2352c920
mno <myonaingoo623@gmail.com>
2024-06-27 23:48:43 +0630

    nav example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MyNavComponent } from './my-nav/my-nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MyNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
