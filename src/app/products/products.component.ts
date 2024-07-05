e921f6ee28f7054d1e5a855d91629525bbeb5099
mno <myonaingoo623@gmail.com>
2024-07-05 21:11:51 +0630

    init

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CardComponent } from '../card/card.component';
+
+@Component({
+  selector: 'app-products',
+  standalone: true,
+  imports: [CardComponent],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.scss'
+})
+export class ProductsComponent {
+
+  items = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]
+
+
+
+
+}
